680c4132dd28789cc4882ada2d9162a4156576d1
eryaaa6 <58668947+eryaaa6@users.noreply.github.com>
2019-12-08 13:57:47 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("653354051236528219")
+setInterval(function() {
+channel.send(`سعد خضر سعد خضر سعد خضر سعد خضر سعد خضر`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
